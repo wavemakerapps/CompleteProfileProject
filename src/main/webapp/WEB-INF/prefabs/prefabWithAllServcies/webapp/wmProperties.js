bf454422e5a16b18803bc765eb51449dc43dd8a6
tejaswi maryala <tejaswi.maryala+62@wavemaker.com>
2018-01-24 16:53:17 +0000

    test New files added - [prefabWithAllServcies] Files modified - [Main,profiles,pom.xml]

---
+++ src/main/webapp/WEB-INF/prefabs/prefabWithAllServcies/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "prefabWithAllServcies",
+  "homePage" : "Main",
+  "name" : "prefabWithAllServcies",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.05"
+};
